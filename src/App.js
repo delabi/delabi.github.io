d5abfb8b8f9fe9e61a02bc0b11854a621a5c19e3
Laban Bwire <bwire.laban@gmail.com>
2022-11-09 19:42:00 +0300

    Added Google Analystics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
+import ReactGA from "react-ga";
+
+ReactGA.initialize('G-2L8J630TGL');
 
 function App() {
    return (
